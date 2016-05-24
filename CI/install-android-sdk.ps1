3975b2daa9b835399034ee3649572dc9c5991415
Caleb Clarke <TheAlmightyBob@users.noreply.github.com>
2016-05-23 23:14:01 -0700

    No longer download google APIs addon for CI build

--- CI/install-android-sdk.ps1
+++ CI/install-android-sdk.ps1
@@ -2,4 +2,3 @@ $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android"
 #$AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android"
 
 echo 'y' | & $AndroidToolPath update sdk -u -a -t android-15
-echo 'y' | & $AndroidToolPath update sdk -u -a -t addon-google_apis-google-15
