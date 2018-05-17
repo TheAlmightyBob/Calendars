81fd23702fe85aa2ed0c19cb44760fc966301c82
JP Hellemons <info@jphellemons.nl>
2018-05-17 11:49:41 +0200

    new day, new try because of this article https://codingforsmarties.wordpress.com/2016/01/21/how-to-version-assemblies-destined-for-nuget/

--- Calendars/Calendars.Plugin.iOSUnified/Properties/AssemblyInfo.cs
+++ Calendars/Calendars.Plugin.iOSUnified/Properties/AssemblyInfo.cs
@@ -10,13 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Plugin.Calendars")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
@@ -32,6 +28,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
-[assembly: AssemblyInformationalVersion("0.6.0-beta")]
+[assembly: AssemblyVersion("0.6.52")]
+[assembly: AssemblyFileVersion("0.6.52")]
+[assembly: AssemblyInformationalVersion("0.6.52-beta")]
