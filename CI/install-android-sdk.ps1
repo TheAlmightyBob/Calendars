5ea99c110f4545b35e6b410bf833b130ee3a1291
Caleb Clarke <TheAlmightyBob@users.noreply.github.com>
2018-05-18 09:47:43 -0700

    Fixed Android CI (#55)
    Add `--use-sdk-wrapper` to bypass warning about using the "android" command, which was swallowing our scripted "yes" that was intended to accept the license agreement.

--- CI/install-android-sdk.ps1
+++ CI/install-android-sdk.ps1
@@ -1,4 +1,4 @@
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android" 
 #$AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android"
 
-echo 'y' | & $AndroidToolPath update sdk -u -a -t android-15
+echo 'y' | & $AndroidToolPath update sdk -u -a -t android-15 --use-sdk-wrapper
